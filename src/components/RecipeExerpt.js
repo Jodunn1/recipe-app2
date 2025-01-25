36024922cd05de43521b61c01457ca9353b32b62
Jordan.dunn <jordandunndeveloping@gmail.com>
2025-01-25 01:12:02 -0800

    Fixed recipes

---
+++ src/components/RecipeExerpt.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { truncateText } from "../helpers/utils.js";
+
+const RecipeExcerpt = ({ recipe, handleSelectRecipe }) => {
+  return (
+    <article className='recipe-card'>
+      <figure>
+        <img src={recipe.image_url} alt={recipe.title} />
+      </figure>
+      <h2>{recipe.title}</h2>
+      <p className='flex-spacing'>Description: {truncateText(recipe.description, 20)}</p>
+      <button onClick={() => handleSelectRecipe(recipe)}>View</button>
+    </article>
+  );
+};
+
+export default RecipeExcerpt;
